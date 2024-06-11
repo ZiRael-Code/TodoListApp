72ee61236ce5925cbb2ae4a6ea6fee8aac547958
ZiReal-code <ZiReal-code>
2024-06-11 17:34:06 +0100

    deploy

--- src/test/java/com/TodoLists/Data/Repository/TodoItemRepoImpliTest.java
+++ src/test/java/com/TodoLists/Data/Repository/TodoItemRepoImpliTest.java
@@ -1,48 +1,48 @@
-package com.TodoLists.Data.Repository;
-
-import com.TodoLists.Data.Model.ToDoItem;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class TodoItemRepoImpliTest {
-    @Autowired
-    TodoItemRepo todoItemRepo;
-
-    @Test
-    void findAllUserTask() throws Exception {
-        System.out.println(todoItemRepo.findAllUserTask(1).toString());
-    }
-
-    @Test
-    void saveTodoItemText() throws Exception {
-        ToDoItem toDoItem = new ToDoItem();
-        toDoItem.setUserId(1);
-        todoItemRepo.save(toDoItem);
-        assertEquals(1,  todoItemRepo.findAllUserTask(1).size());
-    }
-    @Test
-    void deleteAll() throws Exception {
-        todoItemRepo.deleteAll(1);
-        assertEquals(0,  todoItemRepo.findAllUserTask(1).size());
-    }
-
-    @Test
-    void findById() throws Exception {
-        assertNotNull(todoItemRepo.findTask(1,1));
-    }
-
-    @Test
-    void findAllPriority() {
-
-    }
-
-    @Test
-    void deleteTask() throws Exception {
-        todoItemRepo.deleteTask(1, 1);
-        assertEquals(0, todoItemRepo.findAllUserTask(1).size());
-
-    }
-}
+//package com.TodoLists.Data.Repository;
+//
+//import com.TodoLists.Data.Model.ToDoItem;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//@SpringBootTest
+//class TodoItemRepoImpliTest {
+//    @Autowired
+//    TodoItemRepo todoItemRepo;
+//
+//    @Test
+//    void findAllUserTask() throws Exception {
+//        System.out.println(todoItemRepo.findAllUserTask(1).toString());
+//    }
+//
+//    @Test
+//    void saveTodoItemText() throws Exception {
+//        ToDoItem toDoItem = new ToDoItem();
+//        toDoItem.setUserId(1);
+//        todoItemRepo.save(toDoItem);
+//        assertEquals(1,  todoItemRepo.findAllUserTask(1).size());
+//    }
+//    @Test
+//    void deleteAll() throws Exception {
+//        todoItemRepo.deleteAll(1);
+//        assertEquals(0,  todoItemRepo.findAllUserTask(1).size());
+//    }
+//
+//    @Test
+//    void findById() throws Exception {
+//        assertNotNull(todoItemRepo.findTask(1,1));
+//    }
+//
+//    @Test
+//    void findAllPriority() {
+//
+//    }
+//
+//    @Test
+//    void deleteTask() throws Exception {
+//        todoItemRepo.deleteTask(1, 1);
+//        assertEquals(0, todoItemRepo.findAllUserTask(1).size());
+//
+//    }
+//}
--- src/test/java/com/TodoLists/Services/UserServiceImplTest.java
+++ src/test/java/com/TodoLists/Services/UserServiceImplTest.java
@@ -1,109 +1,109 @@
-package com.TodoLists.Services;
-
-import com.TodoLists.DTOs.Request.CreateUserRequest;
-import com.TodoLists.DTOs.Request.LoginRequest;
-import com.TodoLists.DTOs.Response.LoginResponse;
-import com.TodoLists.Data.Model.User;
-import com.TodoLists.Data.Repository.UserRepoImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class UserServiceImplTest {
-    @Autowired
-    private UserServiceImpl userService;
-    @Autowired
-    private UserRepoImpl userRepo;
-    @Test
-    void createAccount() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest();
-        createUserRequest.setUsername("hello");
-        createUserRequest.setPassword("hello123");
-        createUserRequest.setEmail("");
-        LoginResponse loginResponse = userService.createAccount(createUserRequest);
-//        assertEquals(1, userService.findAllUserTask(userId));
-        System.out.println(loginResponse.getId());
-    }
-
-    @Test
-    void findAll() throws Exception {
-        for (User user :userService.findAllUser()){
-            System.out.println(user.getId());
-        }
-    }
-    @Test
-    void logIn() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest();
-        createUserRequest.setUsername("hello");
-        createUserRequest.setEmail("");
-        createUserRequest.setPassword("hello123");
-        userService.createAccount(createUserRequest);
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setUsername("betty");
-        loginRequest.setPassword("hi");
-        ;
-        Assertions.assertTrue(userService.logIn(loginRequest).isLoginStatus());
-    }
-
-    @Test
-    void logOut() throws Exception {
-        userService.logOut("hello");
-        assertFalse(userRepo.findByUsername("hello").isEnable());
-    }
-
-    @Test
-    void deleteAccount() throws Exception {
-        CreateUserRequest createUserRequest = new CreateUserRequest();
-        createUserRequest.setUsername("hello");
-        createUserRequest.setPassword("hello123");
-        userService.createAccount(createUserRequest);
-        userService.deleteAccount(createUserRequest);
-        assertEquals(0, userRepo.findAll().size());
-
-    }
-
-//    @Test
-//    void addTask() throws Exception {
-//        AddTaskRequest newTask = new AddTaskRequest();
-//        newTask.setTitle("Test Task");
-//        newTask.setDescription("Still need to do html");
-//        newTask.setPriority(Priority.MEDIUM);
-//        newTask.setDueDate(LocalDate.now());
-//        int userId = 1;
-//        newTask.setUserId(userId);
+//package com.TodoLists.Services;
 //
-//        LoginRequest createUserRequest = new LoginRequest();
+//import com.TodoLists.DTOs.Request.CreateUserRequest;
+//import com.TodoLists.DTOs.Request.LoginRequest;
+//import com.TodoLists.DTOs.Response.LoginResponse;
+//import com.TodoLists.Data.Model.User;
+//import com.TodoLists.Data.Repository.UserRepoImpl;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//@SpringBootTest
+//class UserServiceImplTest {
+//    @Autowired
+//    private UserServiceImpl userService;
+//    @Autowired
+//    private UserRepoImpl userRepo;
+//    @Test
+//    void createAccount() throws Exception {
+//        CreateUserRequest createUserRequest = new CreateUserRequest();
 //        createUserRequest.setUsername("hello");
 //        createUserRequest.setPassword("hello123");
-//        userService.logIn(createUserRequest);
-//
-//        userService.addTask(newTask);
+//        createUserRequest.setEmail("");
+//        LoginResponse loginResponse = userService.createAccount(createUserRequest);
+////        assertEquals(1, userService.findAllUserTask(userId));
+//        System.out.println(loginResponse.getId());
 //    }
-
-//    @Test
-//    void getAllTasks() throws Exception {
-//        AddTaskRequest newTask = new AddTaskRequest();
-//        newTask.setTitle("Test Task");
-//        newTask.setDescription("Still need to do html");
-//        newTask.setPriority(Priority.MEDIUM);
-//        newTask.setDueDate(LocalDate.now());
-//        newTask.setUserId(1);
 //
-//        LoginRequest createUserRequest = new LoginRequest();
+//    @Test
+//    void findAll() throws Exception {
+//        for (User user :userService.findAllUser()){
+//            System.out.println(user.getId());
+//        }
+//    }
+//    @Test
+//    void logIn() throws Exception {
+//        CreateUserRequest createUserRequest = new CreateUserRequest();
+//        createUserRequest.setUsername("hello");
+//        createUserRequest.setEmail("");
+//        createUserRequest.setPassword("hello123");
+//        userService.createAccount(createUserRequest);
+//        LoginRequest loginRequest = new LoginRequest();
+//        loginRequest.setUsername("betty");
+//        loginRequest.setPassword("hi");
+//        ;
+//        Assertions.assertTrue(userService.logIn(loginRequest).isLoginStatus());
+//    }
+//
+//    @Test
+//    void logOut() throws Exception {
+//        userService.logOut("hello");
+//        assertFalse(userRepo.findByUsername("hello").isEnable());
+//    }
+//
+//    @Test
+//    void deleteAccount() throws Exception {
+//        CreateUserRequest createUserRequest = new CreateUserRequest();
 //        createUserRequest.setUsername("hello");
 //        createUserRequest.setPassword("hello123");
+//        userService.createAccount(createUserRequest);
+//        userService.deleteAccount(createUserRequest);
+//        assertEquals(0, userRepo.findAll().size());
 //
-//        userService.logIn(createUserRequest);
-//        userService.addTask(newTask);
-//
-//        assertEquals(2, userRepo.findByUsername("hello").getMyTask().size());
 //    }
 //
-//@Test
-//    void updateTitle_Description(){
+////    @Test
+////    void addTask() throws Exception {
+////        AddTaskRequest newTask = new AddTaskRequest();
+////        newTask.setTitle("Test Task");
+////        newTask.setDescription("Still need to do html");
+////        newTask.setPriority(Priority.MEDIUM);
+////        newTask.setDueDate(LocalDate.now());
+////        int userId = 1;
+////        newTask.setUserId(userId);
+////
+////        LoginRequest createUserRequest = new LoginRequest();
+////        createUserRequest.setUsername("hello");
+////        createUserRequest.setPassword("hello123");
+////        userService.logIn(createUserRequest);
+////
+////        userService.addTask(newTask);
+////    }
 //
+////    @Test
+////    void getAllTasks() throws Exception {
+////        AddTaskRequest newTask = new AddTaskRequest();
+////        newTask.setTitle("Test Task");
+////        newTask.setDescription("Still need to do html");
+////        newTask.setPriority(Priority.MEDIUM);
+////        newTask.setDueDate(LocalDate.now());
+////        newTask.setUserId(1);
+////
+////        LoginRequest createUserRequest = new LoginRequest();
+////        createUserRequest.setUsername("hello");
+////        createUserRequest.setPassword("hello123");
+////
+////        userService.logIn(createUserRequest);
+////        userService.addTask(newTask);
+////
+////        assertEquals(2, userRepo.findByUsername("hello").getMyTask().size());
+////    }
+////
+////@Test
+////    void updateTitle_Description(){
+////
+////}
 //}
-}
