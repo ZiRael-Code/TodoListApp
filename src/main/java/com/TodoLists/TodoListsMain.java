21e7f5433c64471a246ed19b635daa2e72f83e99
ZiReal-code <ZiReal-code>
2024-06-13 09:46:23 +0100

    env virable

--- src/main/java/com/TodoLists/TodoListsMain.java
+++ src/main/java/com/TodoLists/TodoListsMain.java
@@ -4,17 +4,40 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 @SpringBootApplication
 public class TodoListsMain implements CommandLineRunner{
-    public static final String FILENAME = "C:\\Users\\Israel\\MyFile\\TodoLists\\src\\main\\java\\com\\TodoLists\\user.dat";
+    public static final String FILENAME = System.getenv("FILE_PATH") != null ? System.getenv("FILE_PATH") : "/persistent/file.dat";
     public static void main(String[] args) {
         SpringApplication.run(TodoListsMain.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        File file = new File(FILENAME);
+
+        if (!file.exists()) {
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+           boolean fileCreated =  file.createNewFile();
+//            if (!fileCreated){
+//
+//            }
+
+
+        }
+
+
+
+
+
+
+
+
         Path path = Path.of(FILENAME);
         if (!Files.exists(path)) {
             Files.createFile(path);
