fe904d6a4ffa8760a9e8c01f9753f8e2763722ad
ZiReal-code <ZiReal-code>
2024-09-23 21:32:50 +0100

    commit

--- src/main/java/com/TodoLists/TodoListsMain.java
+++ src/main/java/com/TodoLists/TodoListsMain.java
@@ -10,38 +10,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 @SpringBootApplication
 public class TodoListsMain implements CommandLineRunner{
-    public static final String FILENAME = System.getenv("FILE_PATH") != null ? System.getenv("FILE_PATH") : "C:\\Users\\Israel\\Desktop\\TodoListApp\\src\\main\\java\\user";
     public static void main(String[] args) {
         SpringApplication.run(TodoListsMain.class, args);
     }
+    public static final String FILENAME = System.getenv("FILE_PATH") != null ? System.getenv("FILE_PATH") : "C:\\Users\\Israel\\Desktop\\TodoListApp\\src\\main\\java\\user";
 
     @Override
     public void run(String... args) throws Exception {
 
-        try {
-            File file = new File(FILENAME);
-            if (!file.exists()) {
-                file.createNewFile();
+//        try {
+//            File file = new File(FILENAME);
+//            if (!file.exists()) {
+//                file.createNewFile();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        File file = new File(FILENAME);
+
+        if (!file.exists()) {
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-//        File file = new File(FILENAME);
-//
-//        if (!file.exists()) {
-//            File parentDir = file.getParentFile();
-//            if (parentDir != null && !parentDir.exists()) {
-//                parentDir.mkdirs();
-//            }
-//
-//
-//        }
-//
-//        Path path = Path.of(FILENAME);
-//        if (!Files.exists(path)) {
-//            Files.createFile(path);
-//        }
+        Path path = Path.of(FILENAME);
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
     }
 
     }
