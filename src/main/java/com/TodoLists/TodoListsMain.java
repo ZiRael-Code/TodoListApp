09acb982d57aa8612820e382e6d95d958361f028
ZiReal-code <ZiReal-code>
2024-09-23 22:05:14 +0100

    commit

--- src/main/java/com/TodoLists/TodoListsMain.java
+++ src/main/java/com/TodoLists/TodoListsMain.java
@@ -10,36 +10,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 @SpringBootApplication
 public class TodoListsMain implements CommandLineRunner{
+    public static final String FILENAME = System.getenv("FILE_PATH") != null ? System.getenv("FILE_PATH") : System.getProperty("java.io.tmpdir") + "/user_data.txt";
+
     public static void main(String[] args) {
         SpringApplication.run(TodoListsMain.class, args);
     }
-    public static final String FILENAME = System.getenv("FILE_PATH") != null ? System.getenv("FILE_PATH") : "C:\\Users\\Israel\\Desktop\\TodoListApp\\src\\main\\java\\user";
 
     @Override
     public void run(String... args) throws Exception {
+        try {
+            // File path is now taken from the environment variable or falls back to a temp directory
+            File file = new File(FILENAME);
+            System.out.println("Attempting to create file at: " + file.getAbsolutePath());
 
-//        try {
-//            File file = new File(FILENAME);
-//            if (!file.exists()) {
-//                file.createNewFile();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        File file = new File(FILENAME);
-
-        if (!file.exists()) {
-            File parentDir = file.getParentFile();
-            if (parentDir != null && !parentDir.exists()) {
-                parentDir.mkdirs();
+            // Check if the file exists, if not, create it
+            if (!file.exists()) {
+                System.out.println("File does not exist. Creating new file...");
+                file.createNewFile();
+            } else {
+                System.out.println("File already exists.");
             }
-        }
-
-        Path path = Path.of(FILENAME);
-        if (!Files.exists(path)) {
-            Files.createFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    }
+
+}
